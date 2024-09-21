b41c61e50dea83aa9c191e4c4452341bc42859c9
vikas dayma <vikasdayma900@gmail.com>
2024-09-21 22:17:40 +0530

    e

--- src/Components/core/Ai Meal/Tracker.jsx
+++ src/Components/core/Ai Meal/Tracker.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tracker = () => {
   return (
     <div className='text-4xl '>
-      <h1>Hy  I am Tracker</h1>
+      <h1>Hy  I am 2 Tracker</h1>
     </div>
   )
 }
